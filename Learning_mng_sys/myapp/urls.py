8a33b4436a1506c715e9e118da8f9d4cabeffd6d
Aug2024 <karpemurli3870@gmail.com>
2025-05-31 10:21:16 +0530

    updated

--- Learning_mng_sys/myapp/urls.py
+++ Learning_mng_sys/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', Home, name='root_home'),
 
     path('index/', Index, name='index'),
     path('home/', Home, name='home'),
